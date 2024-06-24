c6674a2ffa3f47e83c08b823d4e8d906c36631d9
seria <seria.ati@gmail.com>
2024-06-24 12:18:32 +0900

    feat: Add language enum

---
+++ akasha/enums.py
@@ -0,0 +1,20 @@
+from enum import StrEnum
+
+__all__ = ("Language",)
+
+
+class Language(StrEnum):
+    ENGLISH = "en"
+    GERMAN = "de"
+    FRENCH = "fr"
+    SPANISH = "es"
+    ITALIAN = "it"
+    JAPANESE = "ja"
+    KOREAN = "ko"
+    PORTUGUESE = "pt"
+    RUSSIAN = "ru"
+    THAI = "th"
+    TURKISH = "tr"
+    VIETNAMESE = "vi"
+    CHINESE_SIMPLIFIED = "zh-CN"
+    CHINESE_TRADITIONAL = "zh-TW"
