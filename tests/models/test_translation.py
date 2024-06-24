f137040c78a2135a36c3fd7274cdc67ca3218c67
seria <seria.ati@gmail.com>
2024-06-24 12:31:22 +0900

    test: Add translation tests

---
+++ tests/models/test_translation.py
@@ -0,0 +1,17 @@
+import akasha
+
+
+async def test_multi_translate() -> None:
+    for lang in akasha.Language:
+        if lang is akasha.Language.ENGLISH:
+            continue
+        async with akasha.AkashaAPI(lang) as api:
+            await api.get_translations(["Kamisato Ayaka", "Mistsplitter Reforged"])
+
+
+async def test_single_translate() -> None:
+    for lang in akasha.Language:
+        if lang is akasha.Language.ENGLISH:
+            continue
+        async with akasha.AkashaAPI(lang) as api:
+            await api.translate("Kamisato Ayaka")
