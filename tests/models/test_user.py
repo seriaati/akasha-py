b6523cea78b0b6fc8426ee5108dc2bfbe98c30b3
seria <seria.ati@gmail.com>
2024-08-11 10:09:03 +0900

    test: Remove get_user test

--- tests/models/test_user.py
+++ tests/models/test_user.py
@@ -4,8 +4,3 @@ import akasha
 async def test_refresh_user() -> None:
     async with akasha.AkashaAPI() as api:
         await api.refresh_user(901211014)
-
-
-async def test_get_user() -> None:
-    async with akasha.AkashaAPI() as api:
-        await api.get_user(901211014)
